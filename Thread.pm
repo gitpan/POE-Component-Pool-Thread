2ad85e8c80ebac2bf109c4aeac009e742052da6a
Scott McCoy <tag@cpan.org>
2004-12-09 11:12:55 +0000

    Import of TAG/POE-Component-Pool-Thread-0.011 from CPAN.
    gitpan-cpan-distribution: POE-Component-Pool-Thread
    gitpan-cpan-version:      0.011
    gitpan-cpan-path:         TAG/POE-Component-Pool-Thread-0.011.tar.gz
    gitpan-cpan-author:       TAG
    gitpan-cpan-maturity:     released

--- Thread.pm
+++ Thread.pm
@@ -17,7 +17,7 @@ use IO::Handle;
 use POE qw( Pipe::OneWay Filter::Line Wheel::ReadWrite );
 use Fcntl;
 
-*VERSION = \0.01;
+*VERSION = \0.011;
 
 use constant DEBUG => 0;
 
@@ -116,9 +116,9 @@ sub new {
             my $iqueue = $heap->{$tid}{iqueue};
 
             if ($rqueue->pending) {
-                if ($opt{Callback}) {
-                    DEBUG && warn "Dispatching Callback";
-                    $opt{Callback}->( @_[0..ARG0-1], @{$rqueue->dequeue} );
+                if ($opt{CallBack}) {
+                    DEBUG && warn "Dispatching CallBack";
+                    $opt{CallBack}->( @_[0..ARG0-1], @{$rqueue->dequeue} );
                 }
             }
 
@@ -250,3 +250,169 @@ sub thread_entry_point {
 }
 
 1;
+
+=head1 NAME
+
+POE::Component::Pool::Thread - A POE Managed Boss/Worker threadpool.
+
+=head1 SYNOPSIS
+
+ use POE qw( Component::Pool::Thread );
+ 
+ POE::Component::Pool::Thread->new
+    ( MinFree       => 2,
+      MaxFree       => 5,
+      MaxThreads    => 15,
+      StartThrneads => 5,
+      Name          => "ThreadPool",
+      EntryPoint    => \&thread_entry_point,
+      CallBack      => \&result_handler,
+      inline_states => {
+          _start => sub {
+               my ($kernel, $heap) = @_[ KERNEL, HEAP ];
+
+               # We are inside the component session
+               $kernel->yield(run => @arguements);
+
+               $kernel->post(ThreadPool => run => @arguements);
+          },
+      }
+    );
+
+ sub thread_entry_point {
+    my (@arguements) = @_;
+
+    return 1;
+ }
+
+ sub result_handler {
+    my ($kernel, $result) = @_[ KERNEL, ARG0 ];
+
+    $result == 1;
+ }
+
+=head1 DESCRIPTION
+
+This is an expand-on-demand thread pool managed through a POE session in a
+manner that does not interfer with cooperative multitasking.  A single pipe is
+created, each thread communicates its state to the main process through this
+pipe.  No serialization occurs (these are threads, not child processes), so
+execution is very fast.
+
+=head1 RATIONALE
+
+Cooperative Co-routine type programming isn't always available.  Some third
+party software (dependant libraries and/or modules) and particular tasks block
+processing weither you like it or not.
+
+Creation of threads is alot of overhead, infact quite a bit more overhead under
+the current implementation of ithreads than fork is.  Allocating these
+resources before you need them is an obvious solution to this problem, if you
+create the threads and re-use them, they're around when you need them without
+the horrendously slow threads->create() method.
+
+Communicating the results of a threads processing requires allowing it to exit.
+This means you will require the overhead of threads->create() next time you
+need to accomplish this task.  With a thread pool designed in this fasion, the
+main thread itself has its own process loop.  The result of each iteration is
+passed through a thread safe queue, allowing you to collect the results of a
+threads execution without the thread exiting.
+
+=head1 CONSTRUCTOR
+
+=over 4
+
+=item new MANY THINGS
+
+The new constructor is the only package method available with this package.  It
+creates a POE thread pool session which you describe in the following
+arguements.
+
+=over 4
+
+=item EntryPoint CODE
+
+This arguement describes the entry point of the thread and is required.  In the
+actual implementation, this is not actually an entry point.  This is instead a
+coderef the thread will call repeatedly.  The arguements of this subroutine
+will be the arguements recieved by the controlling session.  In order to pass
+references as arguements, each reference must be shared (L<threads::shared>).
+Filehandles and blessed references cannot be shared.  You will have to
+translate them yourself.  With file handles, you can pass simply the file
+descriptor and reopen it in the child thread.  With blessed references, you can
+pass the datastructure only, and rebless the reference in the thread.
+
+=item CallBack CODE
+
+This arguement descirbes the result handler, which is where the captured
+results of a threads last execution are sent.  As with EntryPoint subroutine
+arguements, any data structures you wish to pass through return results must be
+explicitly shared (L<threads::shared>).
+
+=item Name ALIAS
+
+This arguement descirbes the default alias your threadpool session is given.
+
+=item StartThreads INTEGER
+
+This arguement describes the number of threads the component will create during
+its "_start" state, or when the POE Session is being started.  This should be a
+number greater than MinFree and less than or equal to MaxFree.
+
+=item MaxThreads INTEGER
+
+This arguement descirbes the maximum number of threads this component will
+create for this task.  If the component is assigned more tasks than threads, it
+will place the remaining tasks in an internal FIFO queue and assign them
+threads as they complete their tasks.
+
+=item MinFree INTEGER
+
+This arguement sets the minimum number of free threads to maintain.  When the
+component is assigned a new task, if there are less than this number of threads
+available, it will yield a request to create a new thread at the components
+convience.
+
+=item MaxFree INTEGER
+
+This arguement provides the maximum number of free threads to maintain.  Upon
+completion of a task, this value is checked.  If there are more free threads
+than this value available, the oldest thread is asked to shut down.
+
+=back
+
+=back
+
+=head1 INLINE STATES
+
+=over 4
+
+=item run LIST
+
+The run state assigns a task to one of the free threads in the pool, or appends
+the task to the components internal FIFO if no threads are available and our
+thread resources are exhausted.
+
+=item shutdown
+
+This state politely asks all threads to exit, deletes the wheel watching the
+one way pipe threads are using to communicate, removes the session alias and
+awaits a clean session shutdown.
+
+=back
+
+=head1 BUGS
+
+Oh I'm pretty sure of it.  If you find some, let me know.
+
+=head1 THANKS
+
+Matt Cashner
+
+Rocco Caputo
+
+=head1 AUTHOR
+
+Scott McCoy (tag@cpan.org)
+
+=cut
